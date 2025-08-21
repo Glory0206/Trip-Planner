1ed2083949d35f77361936db8bc66cfc05ff5320
Yeonggwang <dmb07223@gmail.com>
2025-08-21 22:43:50 +0900

    feat: spring 시작 초기 설정

---
+++ backend/src/main/java/trip/app/controller/AuthController.java
@@ -0,0 +1,12 @@
+package trip.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Trip Planner!";
+    }
+}
